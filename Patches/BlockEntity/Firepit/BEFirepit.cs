34703f6292c839917c85539878444d3391c5046e
LeggyJameh <jamiemills505@gmail.com>
2023-03-22 16:55:56 +0000

    Initial commit

---
+++ Patches/BlockEntity/Firepit/BEFirepit.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using System;
+using Vintagestory.API.Common;
+using Vintagestory.GameContent;
+
+
+namespace BetterFirepit.Patches.BlockEntity.Firepit
+{
+    [HarmonyPatch(typeof(BlockEntityFirepit), nameof(BlockEntityFirepit.smeltItems))]
+    public class BEFirepitSmeltItemsPatch
+    {
+
+        static void Prefix(BlockEntityFirepit __instance, out float __state)
+        {
+            __state = __instance.InputStackTemp;
+
+        }
+        static void Postfix(BlockEntityFirepit __instance, float __state)
+        {
+            __instance.InputStackTemp = __state;
+        }
+    }
+}
