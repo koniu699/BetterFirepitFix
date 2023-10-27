e1c0f28d967115fd8c200b807f824f7a333e01af
LeggyJameh <buh>
2023-10-27 10:02:47 +0100

    Update for game version 1.18.15. Removal of references to .NET 7 build (as all versions are .NET 7 going forward)

--- BetterFirepit/Program.cs
+++ BetterFirepit/Program.cs
@@ -1,19 +1,42 @@
 ﻿using Vintagestory.API.Common;
 using HarmonyLib;
 using Vintagestory.API.Server;
+using Vintagestory.GameContent;
+using System.Reflection;
+using BetterFirepit.Patches.BlockEntity.Firepit;
 
 namespace BetterFirepit
 {
     internal class Program : ModSystem
     {
-        public Harmony harmony;
+        public Harmony? harmony;
 
         public override void StartServerSide(ICoreServerAPI api)
         {
-            harmony = new Harmony("betterfirepit");
-            harmony.PatchAll();
             ILogger logger = api.Logger;
-            logger.Debug("Better Firepit: Patched firepit");
+            harmony = new Harmony("betterfirepit");
+            MethodInfo? original = typeof(BlockEntityFirepit).GetMethod("smeltItems");
+            MethodInfo? prefix = typeof(BEFirepitSmeltItemsPatch).GetMethod("Prefix");
+            MethodInfo? postfix = typeof(BEFirepitSmeltItemsPatch).GetMethod("Postfix");
+
+            if (original != null && prefix != null && postfix != null)
+            {
+                MethodInfo? method = harmony.Patch(original, new HarmonyMethod(prefix), new HarmonyMethod(postfix));
+                if (method != null)
+                {
+                    logger.Debug("Better Firepit: Patched firepit");
+                }
+                else
+                {
+                    logger.Debug("Better Firepit: Failed to patch firepit");
+                }
+            }
+            else
+            {
+                logger.Debug("Better Firepit: Could not find method(s) to patch. More info:");
+                logger.Debug($"{original?.Name ?? "null"}/{prefix?.Name ?? "null"}/{postfix?.Name ?? "null"}");
+            }
+            
             base.StartServerSide(api);
         }
     }
